59a0fc4e6d6b73ba98ae0522afb66260c153748a
Braily Guzman <brailyg@outlook.com>
2025-07-14 14:48:42 -0400

    feat: enhance App component with user authentication and routing setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,48 @@
-import { Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
+import Login from './pages/auth/Login';
+import { useContext, useEffect, useState } from 'react';
+import CurrentUserContext from './context/current-user-context';
+import GuestRoutes from './components/GuestRoutes';
+import Register from './pages/auth/Register';
 
 const App = () => {
-  return <Routes></Routes>;
+  const currentUserContext = useContext(CurrentUserContext);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchCurrentUser = async () => {
+      try {
+        const response = await fetch('/api/auth/me');
+        if (response.ok) {
+          const userData = await response.json();
+          currentUserContext?.setCurrentUser(userData);
+          console.log(userData);
+          console.log(currentUserContext?.currentUser);
+        } else {
+          currentUserContext?.setCurrentUser(null);
+        }
+      } catch (error) {
+        console.error('Failed to fetch current user:', error);
+      }
+
+      setLoading(false);
+    };
+
+    fetchCurrentUser();
+  }, [currentUserContext]);
+
+  if (loading) {
+    return null;
+  }
+
+  return (
+    <Routes>
+      <Route element={<GuestRoutes />}>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Route>
+    </Routes>
+  );
 };
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
 import { BrowserRouter } from 'react-router-dom';
+import CurrentUserContextProvider from './context/CurrentUserContextProvider.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <CurrentUserContextProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </CurrentUserContextProvider>
   </StrictMode>
 );
