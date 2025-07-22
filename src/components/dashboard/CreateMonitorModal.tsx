c13e871001407266d37fd7b744f115a14a563466
Braily Guzman <brailyg@outlook.com>
2025-07-22 15:49:22 -0400

    refactor: remove unused CreateSSLMonitor and CreateDatabaseMonitor components from CreateMonitorModal

--- src/components/dashboard/CreateMonitorModal.tsx
+++ src/components/dashboard/CreateMonitorModal.tsx
@@ -6,9 +6,7 @@ import type {
 } from '../../interfaces/Monitor';
 import { FaTimes } from 'react-icons/fa';
 import CreateHTTPMonitor from './monitors/CreateHTTPMonitor';
-import CreateSSLMonitor from './monitors/CreateSSLMonitor';
 import CreateDNSMonitor from './monitors/CreateDNSMonitor';
-import CreateDatabaseMonitor from './monitors/CreateDatabaseMonitor';
 
 export interface CreateMonitorModalProps {
   projectId: string;
@@ -187,9 +185,7 @@ export function CreateMonitorModal({
               className="w-full bg-gray-700 border border-gray-600 rounded px-3 py-2 text-white focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
             >
               <option value="http">HTTP/HTTPS</option>
-              <option value="ssl">SSL</option>
               <option value="dns">DNS</option>
-              <option value="database">Database</option>
             </select>
           </div>
 
@@ -219,22 +215,8 @@ export function CreateMonitorModal({
             />
           )}
 
-          {formData.type === 'ssl' && (
-            <CreateSSLMonitor
-              formData={formData}
-              onConfigChange={handleConfigChange}
-            />
-          )}
-
           {formData.type === 'dns' && (
             <CreateDNSMonitor
-              formData={formData}
-              onConfigChange={handleConfigChange}
-            />
-          )}
-
-          {formData.type === 'database' && (
-            <CreateDatabaseMonitor
               formData={formData}
               onConfigChange={handleConfigChange}
             />
